3395e4c64121190ec70e35227f632d7598ff0fbd
FpgaPublish <fu3039605879@163.com>
2023-10-26 23:22:06 +0800

    完成了PA的octave部分的自动化，准备modelsim的自动化

---
+++ smart_eye_pa_demo/imag_file.srcs/main_phase.m
@@ -0,0 +1,30 @@
+##################################################################################
+## Company: fpgaPublish
+## Engineer: f
+## 
+## Create Date: 2023/10/26 22:37:21
+## Design Name: main_phase
+## Module Name: 
+## Project Name: 
+## Target Devices: 
+## Tool Versions: 
+## Description: 
+## 
+## Dependencies: 
+##  
+## Revision: 
+## Revision 0.01 - File Created 
+## Additional Comments:
+## 
+##################################################################################
+# ================================================================================
+# pre-function to init env
+addpath("./lib_phase");
+# ================================================================================
+# data calculate and solve
+n_img = file_phase();
+# ================================================================================
+# result display
+
+# ================================================================================
+# assert if get right 
